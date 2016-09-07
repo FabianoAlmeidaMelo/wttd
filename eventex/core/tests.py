69536164969073d846e959c922ee1b2683e5f74e
Fabiano Almeida <fabiano@znc.com.br>
2016-09-07 11:47:49 -0300

    remove arquivos duplicados

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('/')
+
+    def test_get(self):
+        """GET / mest return status code 200"""
+        # response = self.client.get('/')
+        return self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must usse index.html"""
+        # response = self.client.get('/')
+        self.assertTemplateUsed(self.response, 'index.html')
